61c8fd7ed521170b5069b33e87f23af5926a3120
Izzette <isabellcowan@gmail.com>
2015-06-22 14:40:19 -0500

    0.3.2 -- Corrected global variable naming in auto.c

--- src/lib/auto.c
+++ src/lib/auto.c
@@ -8,43 +8,43 @@
 #define LRAND_BITCNT 31
 #define CLR_BIT 0x10
 
-typedef unsigned char uchar_t;
+typedef unsigned char __fra_uchar_t;
 
-typedef struct __cell {
+typedef struct __fra_cell {
 	// state must be 0x0 or 0x4
-	uchar_t state;
-	struct __cell *next;
-} cell_t;
+	__fra_uchar_t state;
+	struct __fra_cell *next;
+} __fra_cell_t;
 
-cell_t *automata = NULL;
-unsigned int auto_size;
+__fra_cell_t *automata = NULL;
+unsigned int __fra_auto_size;
 
-bool is_init = false;
-bool is_seed = false;
+bool __fra_is_init = false;
+bool __fra_is_seed = false;
 
 /* RET:
 0 on success
 ENOMEM if (re/m)alloc ret NULL
 */
 fra_err_t fra_init (unsigned int size) {
-	auto_size = size;
+	__fra_auto_size = size;
 
 	if (automata) {
-		automata = realloc (automata, auto_size * sizeof (cell_t));
+		automata = realloc (automata, __fra_auto_size * sizeof (__fra_cell_t));
 	} else {
-		automata = malloc (auto_size * sizeof (cell_t));
+		automata = malloc (__fra_auto_size * sizeof (__fra_cell_t));
 	}
 
 	if (! automata) return ENOMEM;
 
-	cell_t *cur = automata;
+	__fra_cell_t *cur = automata;
 	int i = 0;
-	while (auto_size > i) {
+	while (__fra_auto_size > i) {
 		cur->state = 0;
-		(cur++)->next = automata + (++i) % auto_size;
+		(cur++)->next = automata + (++i) % __fra_auto_size;
 	}
 
-	is_init = true;
+	__fra_is_init = true;
 
 	return 0;
 }
@@ -57,17 +57,17 @@ fra_err_t fra_seed (time_t seed) {
 	srand48 (seed);
 
 	long v = 0;
-	cell_t *cur = automata;
+	__fra_cell_t *cur = automata;
 
 	int i;
-	for (i = 0; auto_size > i; ++i) {
+	for (i = 0; __fra_auto_size > i; ++i) {
 		if (! i % LRAND_BITCNT) v = lrand48 ();
 
-		cur->state = ((uchar_t)(v & 0x1) ? CLR_BIT : 0x0);
+		cur->state = ((__fra_uchar_t)(v & 0x1) ? CLR_BIT : 0x0);
 		v >>= 1;
 	}
 
-	is_seed = true;
+	__fra_is_seed = true;
 
 	return 0;
 }
@@ -77,7 +77,7 @@ true on initialized
 false othersize
 */
 bool fra_is_init () {
-	return is_init;
+	return __fra_is_init;
 }
 
 /* RET:
@@ -85,7 +85,7 @@ true on initialized
 false othersize
 */
 bool fra_size () {
-	return auto_size;
+	return __fra_auto_size;
 }
 
 /* RET:
@@ -93,7 +93,7 @@ true on initialized
 false othersize
 */
 bool fra_is_seed () {
-	return is_seed;
+	return __fra_is_seed;
 }
 
 // vim : set ts=4 sw=4 noet syn=c :
